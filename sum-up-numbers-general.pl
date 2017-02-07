b269d2d724de9cb7007960cb08ac23f252ebe922
Subhay Manandhar <subhay@Subhays-MacBook-Pro.local>
2017-02-07 18:05:30 -0500

    trying for nested list

--- sum-up-numbers-general.pl
+++ sum-up-numbers-general.pl
@@ -1,2 +1,18 @@
 sum-up-numbers-general([], 0).
 
+sum-up-numbers-general(L, N):-
+	[X|Y] = L,
+	number(X),
+	sum-up-numbers-general(Y, N1),
+	N is N1 + X.
+
+
+sum-up-numbers-general(L, N):-
+	[X|Y] = L,
+	is_list(X),
+
+	sum-up-numbers-general(B, N1),
+	N is N1 + A.
+
+%sum-up-numbers-general(L, N):-
+	
--- sum-up-numbers-simple.pl
+++ sum-up-numbers-simple.pl
@@ -1,11 +1,14 @@
+%base case when the list is empty
 sum-up-numbers-simple([], 0).
 
+%for numeric elements
 sum-up-numbers-simple(L, N):-
 	[X|Y] = L,
 	number(X),
 	sum-up-numbers-simple(Y, N1),
 	N is X + N1.
 
+%handles for non numeric number 
 sum-up-numbers-simple(L, N):-
 	[X|Y] = L,
 	not(number(X)),
