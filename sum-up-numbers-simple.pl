9084d669a64fef82343b9e8d6aa33ccf95186dd8
Subhay Manandhar <subhay@Subhays-MacBook-Pro.local>
2017-02-07 17:45:00 -0500

    trying for non number list elements

--- sum-up-numbers-simple.pl
+++ sum-up-numbers-simple.pl
@@ -6,3 +6,8 @@ sum-up-numbers-simple(L, N):-
 	sum-up-numbers-simple(Y, N1),
 	N is X + N1.
 
+sum-up-numbers-simple(L, N):-
+	[X|Y] = L,
+	not(number(X)),
+	sum-up-numbers-simple(Y, N1),
+	N is X + N1.
