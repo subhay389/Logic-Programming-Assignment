3f93ccbc2dcfba3ba7cee3d4aa454cb663a4da5b
Subhay Manandhar <subhay@Subhays-MacBook-Pro.local>
2017-02-07 22:47:22 -0500

    handled case for non numberic list elements when finding minimum

--- min-above-min.pl
+++ min-above-min.pl
@@ -1,9 +1,21 @@
 min_in_list([Min],Min).                 
 
 min_in_list([H,K|T],M) :-
+    number(H),
+    number(K),
     H =< K,                             
     min_in_list([H|T],M).               
 
 min_in_list([H,K|T],M) :-
+    number(H),
+    number(K),
     H > K,                              
     min_in_list([K|T],M). 
+
+min_in_list([H,K|T],M) :-
+	not(number(H)),
+	min_in_list([K|T],M).
+
+min_in_list([H,K|T],M) :-
+	not(number(K)),
+	min_in_list([H|T],M).
