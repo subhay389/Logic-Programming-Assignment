d9e027a6a2ff101f001c9cbd797608b84061ca52
Subhay Manandhar <subhay@Subhays-MacBook-Pro.local>
2017-02-08 13:19:22 -0500

    succesfully flattening list

--- common-unique-elements.pl
+++ common-unique-elements.pl
@@ -1,25 +1,19 @@
-%common-unique-elements(L1,L2,N).
-
 
 % removing nested loop
-remove_nested([X|Y],[X|_]):-
-  listlen(Y, Length),
-  Length < 1.
 
-remove_nested(List, Single_list) :-
-  [X|Y] = List,
-  not(is_list(X)),
-  remove_nested(Y, S1),
-  append(X,S1,Single_list).
+remove_nested([],[]).
 
-remove_nested(List, Single_list) :-
-  [X|_] = List,
-  is_list(X),
-  [A|B] = X,
-  remove_nested(B, S1),
-  append(A,S1,Single_list).
+remove_nested([Head|InTail],Out) :-
+	remove_nested(Head,Flat_list),
+	remove_nested(InTail,OutTail),
+	append(Flat_list,OutTail,Out).
 
+remove_nested([Head|InTail], [Head|OutTail]) :-
+	Head \= [],
+	Head \= [_|_],
+  remove_nested(InTail,OutTail).
 
+%finding list length
 listlen([], 0 ).
 
 listlen([_|Xs] , L ):-
