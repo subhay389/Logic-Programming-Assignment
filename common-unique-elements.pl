0fb05545f9425498a213be620732f86a39d30640
Subhay Manandhar <subhay@Subhays-MacBook-Pro.local>
2017-02-08 12:59:59 -0500

    trying to remove nested list

--- common-unique-elements.pl
+++ common-unique-elements.pl
@@ -1,1 +1,27 @@
-common-unique-elements(L1,L2,N). 
+%common-unique-elements(L1,L2,N).
+
+
+% removing nested loop
+remove_nested([X|Y],[X|_]):-
+  listlen(Y, Length),
+  Length < 1.
+
+remove_nested(List, Single_list) :-
+  [X|Y] = List,
+  not(is_list(X)),
+  remove_nested(Y, S1),
+  append(X,S1,Single_list).
+
+remove_nested(List, Single_list) :-
+  [X|_] = List,
+  is_list(X),
+  [A|B] = X,
+  remove_nested(B, S1),
+  append(A,S1,Single_list).
+
+
+listlen([], 0 ).
+
+listlen([_|Xs] , L ):-
+	listlen(Xs,N),
+	L is N+1.
